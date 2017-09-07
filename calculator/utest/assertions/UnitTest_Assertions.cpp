569355e35c21e315e6b02ed69803c65216240bb4
Sharma, Vivek <Vivek.Sharma2@harman.com>
2017-09-07 08:18:02 -0700

    renamed noFixtureClass

--- calculator/utest/assertions/UnitTest_Assertions.cpp
+++ calculator/utest/assertions/UnitTest_Assertions.cpp
@@ -1,11 +1,19 @@
 #include "gtest/gtest.h"
 
-TEST(CheckTest, test1) {
+TEST(AssertionTests, expectEqTest) {
 	int a = 0;
 	EXPECT_EQ(a, 0);
+}
+
+TEST(AssertionTests, assertEqTest) {
+	int a = 2;
+	ASSERT_EQ(a, 2);
+	ASSERT_LT(a, 5);
+	ASSERT_GT(a, 1);
 }
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
